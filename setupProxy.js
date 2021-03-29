0c83d40e2f332fd0d5f9de4030cd22df7efb4ef0
KEK-W8 <73126556+KEK-W8@users.noreply.github.com>
2021-03-30 01:46:04 +0300

    deploy

---
+++ setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "https://blooming-dawn-64438.herokuapp.com",
+      changeOrigin: true,
+    })
+  );
+};
